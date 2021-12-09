d00d8ce2278c15918cfddba3f8e5b1ebac5f184e
Lucas Lopes <lucascastro@id.uff.br>
2021-12-09 01:04:13 -0300

    Novas cores, nova lógica de exibição de data de atividade

--- src/mainGraph.js
+++ src/mainGraph.js
@@ -10,7 +10,7 @@ export default class MainGraph {
         // set the dimensions and margins of the graph
         var margin = {top: 10, right: 0, bottom: 30, left: 30},
             width = 1400 - margin.left - margin.right,
-            height = 1200 - margin.top - margin.bottom;
+            height = 1600 - margin.top - margin.bottom;
   
         // append the svg object to the body of the page
         var svg = d3.select("#my_dataviz")
@@ -24,9 +24,9 @@ export default class MainGraph {
       
       this.data.then(function(data){
         console.log(data)
-        // Y axis
+        // X axis
         var x = d3.scaleLinear()
-          .domain([1750, 1970])
+          .domain([1700, 1970])
           .range([0, width]);
         svg.append("g")
           .call(d3.axisBottom(x))
@@ -39,11 +39,11 @@ export default class MainGraph {
         // svg.append("g")
         //   .call(d3.axisLeft(y))
 
-        const yAxisGrid = d3.axisLeft(y).tickSize(-width).tickFormat('').ticks(10);
-        svg.append('g')
-        .attr('class', 'y axis-grid')
-        .attr('color','#FFF')
-        .call(yAxisGrid);
+        // const yAxisGrid = d3.axisLeft(y).tickSize(-width).tickFormat('').ticks(10);
+        // svg.append('g')
+        // .attr('class', 'y axis-grid')
+        // .attr('color','#DDD')
+        // .call(yAxisGrid);
 
         // Tooltips
         var tooltip = d3.select("body").append("div")
@@ -66,7 +66,7 @@ export default class MainGraph {
         .style("position", "absolute")
         .style("text-align", "center")
         .style("width", "200px")
-        .style("height", "58px")
+        .style("height", "38px")
         .style("padding", "8px")
         .style("font", "12px sans-serif")
         .style("pointer-events", "none")
@@ -103,53 +103,107 @@ export default class MainGraph {
           .data(uniqueAuthors)
           .enter()
           .append("line")
-            .attr("x1", function(d) { if (+d.birth_year != "Unknown") {return x(+d.birth_year); }})
-            .attr("x2", function(d) { if (+d.death_year != "Unknown") {return x(+d.death_year); }})
+            .attr("x1", function(d) {return x(d.birth_year); })
+            .attr("x2", function(d) {return x(d.death_year); })
             .attr("y1", function(d) { return y(d.name); })
             .attr("y2", function(d) { return y(d.name); })
-            .attr("stroke", function(d){ if (d.birth_year != "Unknown" || d.death_year != "Unknown") {return "#CCC"} })
+            .attr("stroke", function(d){ if (d.birth_year != "Unknown" && d.death_year != "Unknown") {return "#CCC"} })
             .attr("stroke-width", "4px")
-
-        // Line gradient configuration
+        
         svg.append("linearGradient")
           .data(uniqueAuthors)
-            .attr("id", "line-gradient")
-            .attr("gradientUnits", "userSpaceOnUse")
-            .attr("x1", function(d) { if (+d.birth_year != "Unknown") {return x(+d.birth_year);} else if (+d.birth_year == "Unknown" && +d.death_year != "Unknown") return x(+d.death_year - 80)})
-            .attr("x2", function(d) { if (+d.death_year != "Unknown") {return x(+d.death_year); } else if (+d.death_year == "Unknown" && +d.birth_year != "Unknown") {return x(+d.birth_year + 80)}})
-            .attr("y1", 0)
-            .attr("y2", 0)
-            .selectAll("stop")
-              .data([
-                {offset: "50%", color: "white"},
-                {offset: "100%", color: "black"}
-              ])
-            .enter().append("stop")
-              .attr("offset", function(d) { return d.offset; })
-              .attr("stop-color", function(d) { return d.color; });
-        
+          .attr("id", "line-gradient")
+          .attr("gradientUnits", "userSpaceOnUse")
+          .attr("x1", function(d) { return x(x1(d)) })
+          .attr("x2", function(d) { return x(x2(d)) })
+          .attr("y1", 0)
+          .attr("y2", 0)
+          .selectAll("stop")
+            .data([
+              {offset: "0%", color: "blue"},
+              {offset: "100%", color: "red"}
+            ])
+          .enter().append("stop")
+            .attr("offset", function(d) { return d.offset; })
+            .attr("stop-color", function(d) { return d.color; });
+
+        svg.append("linearGradient")
+          .data(uniqueAuthors)
+          .attr("id", "line-gradient-2")
+          .attr("gradientUnits", "userSpaceOnUse")
+          .attr("x1", function(d) { return x(x1(d)) })
+          .attr("x2", function(d) { return x(x2(d)) })
+          .attr("y1", 0)
+          .attr("y2", 0)
+          .selectAll("stop")
+            .data([
+              {offset: "0%", color: "red"},
+              {offset: "100%", color: "white"}
+            ])
+          .enter().append("stop")
+            .attr("offset", function(d) { return d.offset; })
+            .attr("stop-color", function(d) { return d.color; });
+
         // Gradient lines for entries with unknown birth/death year
         svg.selectAll("myline")
           .data(uniqueAuthors)
           .enter()
           .append("line")
-          .attr("x1", function(d) { if (+d.birth_year != "Unknown") {return x(+d.birth_year);} else if (+d.birth_year == "Unknown" && +d.death_year != "Unknown") return x(+d.death_year - 80)})
-          .attr("x2", function(d) { if (+d.death_year != "Unknown") {return x(+d.death_year); } else if (+d.death_year == "Unknown" && +d.birth_year != "Unknown") {return x(+d.birth_year + 80)}})
-            .attr("y1", function(d) { return y(d.name); })
-            .attr("y2", function(d) { return y(d.name); })
-            .attr("stroke", function(d){ if (d.birth_year == "Unknown" || d.death_year == "Unknown") {return "url(#line-gradient)"}})
-            .attr("stroke-width", "6px")
+          .attr("x1", function(d) { return x(x1(d)) })
+          .attr("x2", function(d) { return x(x2(d)) })
+          .attr("y1", function(d) { return y(d.name); })
+          .attr("y2", function(d) { return y(d.name); })
+          .attr("stroke", function(d){ if (d.birth_year == "Unknown") {return "#CCC"} else if (d.death_year == "Unknown") {return "#CCC"}})
+          .attr("stroke-width", "4px")
+
+        // Gradient lines for active date entries
+        svg.selectAll("myline")
+          .data(uniqueAuthors)
+          .enter()
+          .append("line")
+          .attr("x1", function(d) { return x(x1ActiveDate(d)) })
+          .attr("x2", function(d) { return x(x2ActiveDate(d)) })
+          .attr("y1", function(d) { return y(d.name); })
+          .attr("y2", function(d) { return y(d.name); })
+          .attr("stroke", function() {return "#b84d91"})
+          .attr("stroke-width", "4px")
+          .on("mouseover", function(event, d) {
+            d3.select(this).transition()
+            .duration(400).attr('stroke', '#b84d91').attr("stroke-width", "8px")
+            authorInfo.transition()
+              .duration(400)
+              .style("opacity", .9);
+              authorInfo.html("Data de atividade aproximada: "+d.active_date)
+              .style("left", (event.pageX) + "px")
+              .style("top", (event.pageY) + "px");
+
+            d3.select(this).enter()
+              .append("div")
+                .attr("x1", function(d) { if (d.active_date != "Unknown") {return x(d.active_date - 50); } else { return }})
+                .attr("x2", function(d) { if (d.active_date != "Unknown") {return x(d.active_date + 50); } else { return }})
+                .attr("y1", function(d) { return y(d.name); })
+                .attr("y2", function(d) { return y(d.name); })
+                .style("stroke", function(d){if (d.active_date != "Unknown"){ return "#00FFFF"} else {return "#bbbbbb"}})
+                .attr("stroke-width", "4px")
+          })
+          .on("mouseout", function() {
+            d3.select(this).transition()
+            .duration(400).attr('stroke', '#b84d91').attr("stroke-width", "4px")
+            authorInfo.transition()
+              .duration(400)
+              .style("opacity", 0);
+            });
         
         // Author names
         svg.selectAll("myline")
           .data(uniqueAuthors)
           .enter()
           .append("text")
-            .attr("x", function(d) { if (d.birth_year != "Unknown") {return x(+d.birth_year) + 20; } else if (d.death_year != "Unknown" || d.birth_year == "Unknown") { return  x(+d.death_year - 79 )}})
+            .attr("x", function(d) { if (d.birth_year != "Unknown") {return x(+d.birth_year) + 20; } else if (d.death_year != "Unknown" && d.birth_year == "Unknown") { return  x(+d.death_year - 80)} else if (d.active_date != "Unknown") {return x(x1ActiveDate(d))}})
             .attr("y", function(d) { return y(d.name) -5; })
             .attr('stroke', 'grey')
-            .attr("font-weight", 400)
-            .style("font-size", 14)
+            .attr("font-weight", 200)
+            .style("font-size", 12)
             .text(function(d) { return d.name })
             .on("mouseover", function(event, d) {
               d3.select(this).transition()
@@ -178,16 +232,6 @@ export default class MainGraph {
                 .style("opacity", 0);
               });
 
-        // Background lines
-        svg.selectAll("myline")
-            .data(uniqueAuthors)
-            .enter()
-            .append("line")
-              .attr("x", function(d) { if (+d.birth_year != "Unknown" && +d.death_year != "Unknown") {return x(+d.birth_year) + 20; }})
-              .attr("y", function(d) { return y(d.name); })
-              .attr('stroke', 'grey')
-              .attr("stroke-width", "4px")
-
         // Circles of variable 1
         svg.selectAll("mycircle")
           .data(uniqueAuthors)
@@ -196,24 +240,13 @@ export default class MainGraph {
             .attr("cx", function(d) { if (d.birth_year != "Unknown") {return x(+d.birth_year); } else if (d.death_year != "Unknown" || d.birth_year == "Unknown") { return  x(+d.death_year - 80)}})
             .attr("cy", function(d) { return y(d.name); })
             .attr("r", "6")
-            .style("fill", function(d){if (d.birth_year != "Unknown"){ return "#69b3a2"} else {return "#bbbbbb"}})
+            .style("fill", function(d){if (d.birth_year == "Unknown" && d.death_year == "Unknown") {return "#FFF" } else return dotColor(d.birth_year, "#38c92a")})
             .on("mouseover", function(event,d) {
-              d3.select(this).transition()
-                .duration('200').attr("r", 10).style("fill", function(d){if (d.birth_year != "Unknown"){ return "#69b3a2"} else {return "#bbbbbb"}});
-              tooltip.transition()
-                .duration(200)
-                .style("opacity", .9);
-                tooltip.html("Year of birth" + "<br/>" + d.birth_year)
-                .style("left", (event.pageX) + "px")
-                .style("top", (event.pageY - 28) + "px");
-              })
+              mouseover(this, event,"Ano de nascimento", d.birth_year, tooltip)
+            })
             .on("mouseout", function() {
-              d3.select(this).transition()
-              .duration('200').attr("r", 6).style("fill", function(d){if (d.birth_year != "Unknown"){ return "#69b3a2"} else {return "#bbbbbb"}});
-              tooltip.transition()
-                .duration(200)
-                .style("opacity", 0);
-              });
+              mouseout(this, tooltip)
+            });
           
         // Circles of variable 2
         svg.selectAll("mycircle")
@@ -223,25 +256,94 @@ export default class MainGraph {
             .attr("cx", function(d) { if (d.death_year != "Unknown") {return x(+d.death_year); } else if (d.death_year == "Unknown" || d.birth_year != "Unknown") { return  x(+d.birth_year + 80)}})
             .attr("cy", function(d) { return y(d.name); })
             .attr("r", "6")
-            .style("fill", function(d){if (d.death_year != "Unknown"){ return "#4C4082"} else {return "#bbbbbb"}})
+            .style("fill", function(d){if (d.death_year != "Unknown"){ return "#bf1321"} else {return "#fff"}})
           .on("mouseover", function(event,d) {
-            d3.select(this).transition()
-              .duration('200').attr("r", 10).style("fill", function(d){if (d.death_year != "Unknown"){ return "#4C4082"} else {return "#bbbbbb"}});
-            tooltip.transition()
-              .duration(200)
-              .style("opacity", .9);
-              tooltip.html("Year of death" + "<br/>" + d.death_year)
-              .style("left", (event.pageX) + "px")
-              .style("top", (event.pageY - 28) + "px");
-            })
+            mouseover(this, event,"Ano de morte", d.death_year, tooltip)
+          })
           .on("mouseout", function() {
-            d3.select(this).transition()
-              .duration('200').attr("r", 6).style("fill", function(d){if (d.death_year != "Unknown"){ return "#4C4082"} else {return "#bbbbbb"}});
-            tooltip.transition()
-              .duration(200)
-              .style("opacity", 0);
-            });
+            mouseout(this, tooltip)
+          });
       })
         
     }
+}
+
+// auxiliary functions
+function x1(d) { 
+  if (d.birth_year != "Unknown") {
+    return +d.birth_year;
+  } else if (d.birth_year == "Unknown" && d.death_year != "Unknown") {
+    return +d.death_year - 80;
+  }
+}
+
+function x2(d) { 
+  if (d.death_year != "Unknown") {
+    return +d.death_year;
+  } else if (d.death_year == "Unknown" && d.birth_year != "Unknown") {
+    return +d.birth_year+ 70;
+  }
+}
+
+function x1ActiveDate(d) { 
+  if (d.birth_year == "Unknown" && d.death_year == "Unknown" && d.active_date != "Unknown") {
+    if (d.active_date == "16th century") {
+      return +1500;
+    } else if (d.active_date == "17th century"){
+      return +1600;
+    } else if (d.active_date == "18th century") {
+      return +1700;
+    } else if (d.active_date == "19th century"){
+      return +1800;
+    } else if (d.active_date == "20th century") {
+      return +1900;
+    } else {
+      return +d.active_date - 20;
+    }
+  }
+}
+
+function x2ActiveDate(d) { 
+  if (d.birth_year == "Unknown" && d.death_year == "Unknown" && d.active_date != "Unknown") {
+    if (d.active_date == "16th century") {
+      return +1599;
+    } else if (d.active_date == "17th century"){
+      return +1699;
+    } else if (d.active_date == "18th century") {
+      return +1799;
+    } else if (d.active_date == "19th century"){
+      return +1899;
+    } else if (d.active_date == "20th century") {
+      return +1999;
+    } else {
+      return +d.active_date + 20;
+    }
+  }
+}
+
+function mouseover(context, event, text, data, tooltip) {
+  d3.select(context).transition()
+    .duration('200').attr("r", 10);
+    tooltip.transition()
+      .duration(200)
+      .style("opacity", .9);
+      tooltip.html(text + "<br/>" + data)
+      .style("left", (event.pageX) + "px")
+      .style("top", (event.pageY - 28) + "px");
+}
+
+function mouseout(context, tooltip) {
+  d3.select(context).transition()
+    .duration('200').attr("r", 6);
+    tooltip.transition()
+      .duration(200)
+      .style("opacity", 0);
+}
+
+function dotColor(data, color){
+  if (data != "Unknown"){ 
+    return color
+  } else {
+    return "#FFF"
+  }
 }
